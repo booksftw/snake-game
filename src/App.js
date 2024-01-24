9b91e6b101c9166542f26070c8c9598b0579a166
Nicholas Zachary <39927058+booksftw@users.noreply.github.com>
2024-01-23 22:07:44 -0800

    Init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,101 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
+const initSnake = {
+  bodyLocation: [
+    [0, 0],
+    [1, 0],
+    [2, 0],
+    [3, 0],
+    [4, 0],
+  ],
+  scale: 20,
+  direction: 'DOWN',
+  speed: 1000,
+}
+
 function App() {
+  const [snake, setSnake] = useState(initSnake)
+
+  function move() {
+    const newBodyLocation = [...snake.bodyLocation];
+    const head = snake.bodyLocation[snake.bodyLocation.length - 1];
+
+    if (snake.direction === 'RIGHT') {
+      const x = head[0] + 1;
+      const y = head[1];
+      newBodyLocation.push([x, y])
+    } else if (snake.direction === 'LEFT') {
+      const x = head[0] - 1;
+      const y = head[1];
+      newBodyLocation.push([x, y]);
+    } else if (snake.direction === 'UP') {
+      const x = head[0];
+      const y = head[1] - 1;
+      newBodyLocation.push([x, y]);
+    } else if (snake.direction === 'DOWN') {
+      const x = head[0];
+      const y = head[1] + 1;
+      newBodyLocation.push([x, y]);
+    }
+    console.log("move")
+
+
+    newBodyLocation.shift();
+    console.log(newBodyLocation);//debug
+    setSnake({ ...snake, bodyLocation: newBodyLocation });
+  }
+
+  function onKeyPress(event) {
+    // console.log(event);
+    console.log(event.key);
+
+    if (event.key === "ArrowUp") {
+      setSnake({ ...snake, direction: 'UP' });
+    } else if (event.key === "ArrowDown") {
+      setSnake({ ...snake, direction: 'DOWN' });
+    } else if (event.key === "ArrowRight") {
+      setSnake({ ...snake, direction: 'RIGHT' });
+    } else if (event.key === "ArrowLeft") {
+      setSnake({ ...snake, direction: 'LEFT' });
+    }
+  }
+
+  function hasHitWall() {
+
+  }
+
+  useEffect(() => {
+    const interval = setInterval(move, snake.speed);
+    return () => clearInterval(interval);
+  },);
+
+  useEffect(() => {
+    document.addEventListener('keydown', onKeyPress);
+    return () => document.removeEventListener('keydown', onKeyPress);
+  })
+
+  setTimeout(() => {
+    // move()
+  }, 1000);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='game-board'>
+        {/* <div className='snake-body' style={{ left: 0, top: 0 }}></div>
+        <div className='snake-body' style={{ left: 20, top: 0 }}></div>
+        <div className='snake-body' style={{ left: 40, top: 0 }}></div>
+        <div className='snake-body' style={{ left: 60, top: 0 }}></div> */}
+
+
+        {snake.bodyLocation.map((item) => {
+          return <div className='snake-body' style={{ left: item[0] * snake.scale, top: item[1] * snake.scale }}></div>
+        })}
+
+      </div>
+
+    </>
   );
 }
 
